a0c112a506c2ef9c24337f89dddc5700d61cc401
JhonataOvirowa <jhonataovirowa@gmail.com>
2024-07-21 11:46:38 -0300

    Linkando>>

---
+++ aula1.js
@@ -0,0 +1,11 @@
+const subtrair = function(x, y) {
+    let resultado = x - y
+    console.log(`${x} - ${y} = ${resultado}`)
+    return resultado
+}
+
+const r = subtrair(10, 10)
+
+console.log(`o resultado foi ${r}`)
+
+console.log(subtrair)
